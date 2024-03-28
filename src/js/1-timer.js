b3f94235ba29b2ca6ba30a6a6b789fb789cf597e
JuShapiro <yuliashepel19@gmail.com>
2024-03-28 14:08:46 +0100

    flatpickr

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,12 +1,65 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const options = {
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+
+const input = document.querySelector('#datetime-picker');
+const btn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+
+let userSelectedDate;
+
+
+flatpickr(input, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (userSelectedDate > currentDate) {
+      btn.disabled = false;
+    } else {
+      btn.disabled = true;
+      iziToast.show({
+        title: 'Error',
+        message: 'illegal options',
+        position: 'topCenter',
+        backgroundColor: '#ef4040',
+        color: '#fff',
+      });
+    }
   },
-};
+});
+
+btn.addEventListener("click", startBtn)
+
+function startBtn() {
+  
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
