580da23a4e9926d4b4a5c2420b348c37263aaec1
JuShapiro <yuliashepel19@gmail.com>
2024-03-28 16:02:22 +0100

    not work

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -12,27 +12,25 @@ const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
 
-
+const currentDate = new Date();
 let userSelectedDate;
 
-
-flatpickr(input, {
+let flatpickrInstance = new flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
 
-  onClose(selectedDates) {
+  onClose: function (selectedDates) {
     userSelectedDate = selectedDates[0];
-    const currentDate = new Date();
     if (userSelectedDate > currentDate) {
       btn.disabled = false;
     } else {
       btn.disabled = true;
       iziToast.show({
         title: 'Error',
-        message: 'illegal options',
-        position: 'topCenter',
+        message: 'Illegal options',
+        position: 'topRight',
         backgroundColor: '#ef4040',
         color: '#fff',
       });
@@ -40,26 +38,33 @@ flatpickr(input, {
   },
 });
 
-btn.addEventListener("click", startBtn)
+function updateCounter() {
+  const diff = userSelectedDate - currentDate;
+  const { days, hours, minutes, seconds } = convertMs(diff);
 
-function startBtn() {
-  
+  days.textContent = addLeadingZero(days);
+  hours.textContent = addLeadingZero(hours);
+  minutes.textContent = addLeadingZero(minutes);
+  seconds.textContent = addLeadingZero(seconds);
 }
+
+btn.addEventListener("click", () => setInterval(updateCounter, 1000));
 
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
 
-function convertMs(ms) {
+function convertMs(diff) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = Math.floor(diff / day);
+  const hours = Math.floor((diff % day) / hour);
+  const minutes = Math.floor((diff % hour) / minute);
+  const seconds = Math.floor((diff % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
+
