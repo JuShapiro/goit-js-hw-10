e9dede320ab89b121e6192ff133854f42c79b574
JuShapiro <149778037+JuShapiro@users.noreply.github.com>
2024-03-30 01:39:22 +0000

    Deploying to gh-pages from @ JuShapiro/goit-js-hw-10@d740a0fc07a9bf686d5c3959d67cadb8bdf6fc83 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-ab9e8793.js";import"./assets/vendor-77e16229.js";const e={form:document.querySelector(".form"),delay:document.querySelector(".delay"),states:document.querySelector(".state"),button:document.querySelector(".btn-form")};e.form.addEventListener("submit",t=>{t.preventDefault(),e.delay.value});
+//# sourceMappingURL=commonHelpers2.js.map
